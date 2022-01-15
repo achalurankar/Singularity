02ee60e4f5d79ca283e557c5cf27b8a8ab60ee45
Achal Urankar <achal_urankar@persistent.com>
2022-01-16 00:50:58 +0530

    adapter moved to other package

--- app/src/main/java/com/android/singularity/activity/TaskList.java
+++ app/src/main/java/com/android/singularity/activity/TaskList.java
@@ -1,23 +1,16 @@
 package com.android.singularity.activity;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -26,6 +19,7 @@ import com.android.singularity.R;
 import com.android.singularity.util.Constants;
 import com.android.singularity.util.DateTime;
 import com.android.singularity.util.EventDispatcher;
+import com.android.singularity.util.ListAdapter;
 import com.andromeda.callouts.CalloutManager;
 import com.andromeda.callouts.Session;
 import com.google.android.material.snackbar.Snackbar;
@@ -34,13 +28,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class TaskList extends AppCompatActivity {
+public class TaskList extends AppCompatActivity implements ListAdapter.OnItemClickListener{
 
     private static final String TAG = "TaskList";
     RecyclerView mRecyclerView;
     TextView DateTV, DayTV;
     static JSONObject selectedTask;
-    CustomAdapter mAdapter;
+    ListAdapter mAdapter;
     LinearLayout NoResultsLayout;
     JSONArray mList;
 
@@ -65,8 +59,16 @@ public class TaskList extends AppCompatActivity {
         EventDispatcher.addEventListener(this::getTasks);
     }
 
+    @Override
+    public void onClick(JSONObject clickedItem) {
+        selectedTask = clickedItem;
+        startActivity(new Intent(getApplicationContext(), TaskEditor.class));
+        overridePendingTransition(0, 0);
+    }
+
     public void setRecyclerViewAdapter(JSONArray jsonArray) {
-        mAdapter = new CustomAdapter(TaskList.this, jsonArray);
+        mAdapter = new ListAdapter(TaskList.this, jsonArray);
+        mAdapter.addOnItemClickListener(TaskList.this);
         mRecyclerView.setAdapter(mAdapter);
     }
 
@@ -154,116 +156,6 @@ public class TaskList extends AppCompatActivity {
         }
     }
 
-    public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
-        public final Context mContext;
-        JSONArray mList;
-
-        public CustomAdapter(Context context, JSONArray list) {
-            mContext = context;
-            mList = list;
-        }
-
-        public JSONObject get(int index) {
-            JSONObject object = null;
-            try {
-                object = this.mList.getJSONObject(index);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-            return object;
-        }
-
-        public void remove(int index) {
-            mList.remove(index);
-        }
-
-        @SuppressLint("NotifyDataSetChanged")
-        public void add(int index, JSONObject item) {
-            JSONArray jsonArray = new JSONArray();
-            if(index == mList.length()) {
-                mList.put(item);
-                notifyDataSetChanged();
-                return;
-            }
-            for (int i = 0; i < mList.length(); i++) {
-                if(i == index) {
-                    jsonArray.put(item);
-                }
-                try {
-                    jsonArray.put(mList.getJSONObject(i));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-            mList = jsonArray;
-            notifyDataSetChanged();
-        }
-
-        @NonNull
-        @Override
-        public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(mContext).inflate(R.layout.tasks_item, parent, false);
-            return new CustomViewHolder(v);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull CustomViewHolder holder, final int position) {
-            JSONObject task;
-            try {
-                task = mList.getJSONObject(position);
-                holder.Name.setText(task.getString("Name"));
-                try {
-                    holder.Description.setText(task.getString("Description__c"));
-                } catch (JSONException e) {
-                    holder.Description.setText("No description provided");
-                }
-                holder.Item.setOnClickListener(v -> {
-                    selectedTask = task;
-                    startActivity(new Intent(getApplicationContext(), TaskEditor.class));
-                    overridePendingTransition(0, 0);
-                });
-                String tt = task.getString("Display_Date_Time__c");
-                String[] tt_arr = tt.split(" ");
-                tt = tt_arr[0] + " " + tt_arr[1] + " " + tt_arr[2] + "\n     " + tt_arr[3];
-                holder.Time.setText(tt);
-                holder.CompleteBtn.setOnClickListener(v -> setComplete(task));
-                //check status of task
-                if (task.getBoolean("Is_Completed__c")) {
-                    holder.CompleteBtn.setVisibility(View.INVISIBLE);
-                    holder.TaskStatus.setBackground(ContextCompat.getDrawable(mContext, R.drawable.completed_status));
-                } else
-                    holder.TaskStatus.setBackground(ContextCompat.getDrawable(mContext, R.drawable.pending_status));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
-
-        @Override
-        public int getItemCount() {
-            return mList.length();
-        }
-
-
-        public class CustomViewHolder extends RecyclerView.ViewHolder {
-
-            View TaskStatus;
-            TextView Name, Description, Time;
-            RelativeLayout Item;
-            ImageView CompleteBtn;
-
-            public CustomViewHolder(@NonNull View itemView) {
-                super(itemView);
-                Item = itemView.findViewById(R.id.item);
-                CompleteBtn = itemView.findViewById(R.id.complete_btn);
-                TaskStatus = itemView.findViewById(R.id.task_status);
-                Name = itemView.findViewById(R.id.name);
-                Description = itemView.findViewById(R.id.description);
-                Time = itemView.findViewById(R.id.time);
-            }
-        }
-
-    }
 
     private void setComplete(JSONObject task) {
         try {
