de02a6e096e2283187a15a1ec67e8a95260d378e
Achal Urankar <achalurankar@gmail.com>
2022-08-10 11:55:42 +0530

    creds removed

--- app/src/main/java/com/android/singularity/util/Constants.java
+++ app/src/main/java/com/android/singularity/util/Constants.java
@@ -11,11 +11,11 @@ public class Constants {
     public static final int TYPE_EMAIL = 3;
 
     //credentials
-    private static final String USERNAME = "achal_urankar@playful-otter-5orsso.com";
-    private static final String PASSWORD = "achal@1234";
-    private static final String SECURITY_TOKEN = "at1h6pqzoKK4tnNnIp0gKvoJ";
-    private static final String CLIENT_ID = "3MVG9pRzvMkjMb6mVmRLS1jKALgWzMgGDQhnBh4MfyEQv_ATznjlpPw9ffz3Ur.ZxRF3K6347v7GnsdgUeuJh";
-    private static final String CLIENT_SECRET = "6781FB6C8723F54B5C352552D8386E7ADD2CFB9F9D863C627F15155AF7E82DA4";
+    private static final String USERNAME = "your_username";
+    private static final String PASSWORD = "your_password";
+    private static final String SECURITY_TOKEN = "your_security_token";
+    private static final String CLIENT_ID = "your_client_id";
+    private static final String CLIENT_SECRET = "your_client_secret";
 
     // api endpoints
     public static final String ACCESS_TOKEN_ENDPOINT = "https://login.salesforce.com/services/oauth2/token?grant_type=password&client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET +"&username=" + USERNAME + "&password=" + PASSWORD + SECURITY_TOKEN;
