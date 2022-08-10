72bbc3f48836880a16fb30da19f8db4541dc1ce9
Achal Urankar <achalurankar@gmail.com>
2022-08-10 12:14:25 +0530

    creds removed

--- app/src/main/java/com/android/singularity/util/Credentials.java
+++ app/src/main/java/com/android/singularity/util/Credentials.java
@@ -3,10 +3,9 @@ package com.android.singularity.util;
 public class Credentials {
 
     //credentials
-    public static final String USERNAME = "achal_urankar@playful-otter-5orsso.com";
-    public static final String PASSWORD = "achal1234";
-    public static final String SECURITY_TOKEN = "DwUgVo0kIfDgVp8KaGIzf4qh";
-    public static final String CLIENT_ID = "3MVG9pRzvMkjMb6mVmRLS1jKALgWzMgGDQhnBh4MfyEQv_ATznjlpPw9ffz3Ur.ZxRF3K6347v7GnsdgUeuJh";
-    public static final String CLIENT_SECRET = "6781FB6C8723F54B5C352552D8386E7ADD2CFB9F9D863C627F15155AF7E82DA4";
-
+    public static final String USERNAME = "your_username";
+    public static final String PASSWORD = "your_password";
+    public static final String SECURITY_TOKEN = "your_security_token";
+    public static final String CLIENT_ID = "your_client_id";
+    public static final String CLIENT_SECRET = "your_client_secret";
 }
